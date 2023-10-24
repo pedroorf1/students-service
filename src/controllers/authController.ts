b8c9cab6ac1bb0bacd9c9b558578b28425aa69bd
pedroorf <pedroorf@gmail.com>
2023-10-24 18:36:26 -0300

    test authController

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,8 +4,8 @@ import { TControllers } from "../types/controllersTypes";
 
 export const atuthenticate: TControllers = async (req, res) => {
   const data: ILogin = {
-    email: String(req?.query.email),
-    password: String(req?.query.password),
+    email: String(req?.query?.email),
+    password: String(req?.query?.password),
   };
 
   login(data).then(result => {
@@ -14,7 +14,11 @@ export const atuthenticate: TControllers = async (req, res) => {
 };
 
 export const validation: TControllers = async (req, res) => {
-  const token = String(req?.query.token);
+  const token = String(req?.query?.token);
+
+  if (!token) {
+    return res.send({ error: "Dados invalidos!" });
+  }
   const result = await validate({ token });
   return res.send(result);
 };
